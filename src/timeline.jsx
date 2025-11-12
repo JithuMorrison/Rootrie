853ce710ba218d1f64130483b6f73ef4af7f31be
JithuMorrison <jithus2004@gmail.com>
2025-11-12 08:16:19 +0530

    feat: corrected evo

--- src/timeline.jsx
+++ src/timeline.jsx
@@ -947,7 +947,7 @@ const EvolutionChartMaker = ({ project, nodes, connections, onUpdateProject, onB
           display: flex;
           gap: 10px;
           align-items: center;
-          margin-right: 290px;
+          margin-right: 120px;
         }
         
         .btn {
